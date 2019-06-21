7f084d0c11d4d4dcf61bcf9c010c0b3009224611
heineperson <katie.heineman1@gmail.com>
2019-06-21 03:32:54 +0000

    starting regex code

---
+++ CorrectlyRemixedAbiesNewick.R
@@ -0,0 +1,3 @@
+x <- read.newick(text="((Abies_lasiocarpa:4.274552,Abies_bracteata:4.274552):0.274022,((Abies_grandis:1.585419,Abies_concolor:1.585419):1.233884,
+                 (Abies_amabilis:2.451525,(Abies_procera:0.579571,(Abies_magnifica_var_mag:0.25, Abies_magnifica_var_shastenis:0.25):0.579571):1.871954):0.367778):1.729271);")
+plot(x)
