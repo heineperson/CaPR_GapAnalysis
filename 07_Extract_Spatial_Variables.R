b81584c48a1615973686e6ba83dfb6a8cb5b2db6
heineperson <katie.heineman1@gmail.com>
2019-08-09 17:46:23 +0000

    adding federal lands extraction

--- 07_Extract_Spatial_Variables.R
+++ 07_Extract_Spatial_Variables.R
@@ -21,6 +21,7 @@ source("caspioFunctions.R")
 
 # Reading in Jepson File
 jepson <- readOGR(dsn="Data/JepsonLayer",layer="Geographic_Subdivisions_of_California_TJMII_v2_060415")
+jepson <- spTransform(jepson,CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 
 # Reading in old cnddb point file (can't uplaod the more recent file)
 #cnddb <- fread("Data/tblCNDDBOccurrencesRare_2019-Jun-21_1830.csv")
@@ -36,7 +37,9 @@ coordinates(points_dat) <-  ~V1 + V2
 proj4string(points_dat) <- CRS("+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
 points_dat <- spTransform(points_dat,CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 
-#write.csv(points_dat, "Data/cnddb_PointsfromShape_Aug2019.csv")
+# Federal Lands
+federal_lands <- readOGR(dsn="Data/FederalLands",layer="Federal_Lands_California")
+federal_lands <- spTransform(federal_lands,CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 
 # Reading in Accessions Data
 # Getting more than 1000 rows! This is for the CaPR Accessions List
@@ -56,23 +59,18 @@ CaPRAcc <- TableCombined
 
 #########################
 ## Extracting Jepson for CNDDB File
-# cnddb_noNA <- cnddb[!is.na(Longitude)]
-# coordinates(cnddb_noNA) <-  ~Longitude + Latitude
-# 
-# # Transforming Coordinate Reference Systems
-# proj4string(cnddb_noNA) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
-# 
-# spTransform(cnddb_noNA,CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
-
-#proj4string(jepson) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
-jepson <- spTransform(jepson,CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
-
-#Extracting ecoregions from jepson layer onto the CNDDB occurrences
 a<-over(points_dat,jepson)
 jepCnddb <- cbind(as.data.table(points_dat@data)[,.(EONDX)], as.data.table(a))
 
 # Merging Jepson onto overal cnddb file
 cnddb <- merge(points_dat, jepCnddb, by.x="EONDX",by.y="EONDX",all.x=T)
+
+## Extracting Federal Lands for CNDDB File (although this is also present in the OWNERMGT field)
+b <- over(points_dat,federal_lands)
+fedCnddb <-  cbind(as.data.table(points_dat@data)[,.(EONDX)], as.data.table(b))
+
+# Merging Federal Lands onto cndbb file
+cnddb <- merge(cnddb, fedCnddb, by.x="EONDX",by.y="EONDX",all.x=T)
 
 write.csv(cnddb,"caprPrioritizationApp/AppData/cnddbwithJepson.csv")
 
@@ -83,31 +81,20 @@ coordinates(capr_noNA) <-  ~decimalLongitude + decimalLatitude
 
 # Transforming Coordinate Reference Systems
 proj4string(capr_noNA) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
-
 spTransform(capr_noNA,CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
 
 # Extracting ecoregions from jepson layer onto the CNDDB occurrences
 a<-over(capr_noNA,jepson)
 jepCapr <- cbind(as.data.table(capr_noNA@data)[,.(eventID)], as.data.table(a))
 
+## Extracting Federal Lands for CNDDB File (although this is also present in the OWNERMGT field)
+c <- over(capr_noNA,federal_lands)
+fedCapr <-  cbind(as.data.table(capr_noNA@data)[,.(eventID)], as.data.table(c))
+
 # Merging Jepson onto overal cnddb file
 CaPRAcc <- merge(CaPRAcc, jepCapr, by.x="eventID",by.y="eventID",all.x=T)
+CaPRAcc <- merge(CaPRAcc, fedCapr, by.x="eventID",by.y="eventID",all.x=T)
 
 write.csv(CaPRAcc,"caprPrioritizationApp/AppData/CaPRwithJepson.csv")
 
-# Possible land owners
-BLM
-DOD
-DPR
-PVT
-MWD
-TNC
-USFS
-NPS
-LADWP
-CITY
-CDF
-COUNTY
-DFG
-NM
 
