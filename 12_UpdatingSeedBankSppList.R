2bd8b8c7bfdbd8c141843858217f34a51d69eab6
heineperson <katie.heineman1@gmail.com>
2019-09-24 04:53:46 +0000

    seed bank update

--- 12_UpdatingSeedBankSppList.R
+++ 12_UpdatingSeedBankSppList.R
@@ -16,3 +16,19 @@ source("caspioFunctions.R")
 sdzgOcc <- caspio_get_table_all("Occurrence")
 sdzgSpp <- caspio_get_table_all("PlantNames")
 caprSpp <- caspio_get_table_all("tblSpeciesJepCAPR")
+
+# Creating a summarized table of occurrence by plantname ID
+summ <- sdzgOcc[,.(Count=.N),by="PlantNameID"]
+
+# Mering plant names onto jepIDs
+sdzgSppOccOnly <- merge(sdzgSpp,summ,by="PlantNameID")
+
+# Adding plant name IDs onto caprSpp
+caprsppWithPNI <- merge(caprSpp,sdzgSppOccOnly[,.(taxonIdCAPR,PlantNameID)],by.x="JepID",by.y="taxonIdCAPR",all.x=T)
+
+
+setnames(caprsppWithPNI, c("JepID","family","genus","species","infraspecific_rank","infraspecific_epithet","species_author","infraspecific_author","synonyms","common","nativity"), 
+         c("taxonIdCAPR","Family","Genus","Species","InfraspecificRank","InfraspecificEpithet","SpeciesAuthor","InfraspecificAuthor","Synonomy","CommonName","Nativity"))
+
+write.csv(caprsppWithPNI,"Data/speciesListUpdate.csv")
+
